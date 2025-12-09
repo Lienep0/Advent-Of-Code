f145e4d03b6bc0578c4786a1b143f5c7d9df4d6a
Lienep0 <neilp@live.fr>
2025-12-09 15:38:33 +0100

    Nice visualization

--- 2025/Day-09/contour_plot.py
+++ 2025/Day-09/contour_plot.py
@@ -3,8 +3,6 @@ import matplotlib.pyplot as plt
 with open("input.txt", "r") as f:
     red_tiles = [tuple(map(int, line.strip().split(","))) for line in f.readlines()]
 
-red_tiles += [red_tiles[0]]
-
 x, y = zip(*red_tiles)
 
 plt.fill(x, y)
