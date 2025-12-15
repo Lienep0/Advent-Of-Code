b0d5e080ec12ee8dcdf542b23b3bb69cf22598bf
Lienep0 <neilp@live.fr>
2025-12-15 12:51:11 +0100

    Incomplete day 12

--- 2023/Day-12/part1.py
+++ 2023/Day-12/part1.py
@@ -7,7 +7,36 @@ with open("input.txt", "r") as f:
         record = list(map(int, record.split(',')))
         lines.append((springs, record))
 
-def full_solution(springs, record):
+def is_bad(springs, record):
+    i = 0
+    group_index = 0
+    on_group = False
+    while i < len(springs):
+        if springs[i] == "?":
+            return False
+        if springs[i] == "#" and not on_group:
+            start = i
+            on_group = True
+            if group_index >= len(record):
+                return True
+        elif springs[i] == "." and on_group:
+            on_group = False
+            group_size = i - start
+            if record[group_index] != group_size:
+                return True
+            group_index += 1
+        i += 1
+    
+    # Check end group
+    if on_group:
+        group_size = i - start
+        if record[group_index] != group_size:
+            return True
+        group_index += 1
+
+    return group_index != len(record)
+
+def is_full_solution(springs, record):
     i = 0
     group_index = 0
     on_group = False
@@ -34,17 +63,20 @@ def full_solution(springs, record):
 
     return group_index == len(record)
 
-def bruteforce(springs, record, unkown_count, position=0):
+def smartforce(springs, record, unkown_count, position=0):
+    if is_bad(springs, record):
+        return False
+
     if unkown_count == 0:
-        return full_solution(springs, record)
+        return is_full_solution(springs, record)
 
     while springs[position] != "?":
         position += 1
     
-    return (bruteforce(springs[:position] + "." + springs[position + 1:], record, unkown_count - 1, position + 1) 
-            + bruteforce(springs[:position] + "#" + springs[position + 1:], record, unkown_count - 1, position + 1))
+    return (smartforce(springs[:position] + "." + springs[position + 1:], record, unkown_count - 1, position + 1) 
+            + smartforce(springs[:position] + "#" + springs[position + 1:], record, unkown_count - 1, position + 1))
 
 total = 0
 for springs, record in tqdm.tqdm(lines):
-    total += bruteforce(springs, record, springs.count("?"))
+    total += smartforce(springs, record, springs.count("?"))
 print(total)
--- 2023/Day-12/part2.py
+++ 2023/Day-12/part2.py
@@ -1,3 +1,18 @@
+import tqdm
+
+lines = []
 with open("easyinput.txt", "r") as f:
     for line in f.readlines():
-        print(line)
+        springs, record = line.strip().split(" ")
+        springs = (springs + "?") * 4 + springs
+        record = tuple(map(int, record.split(','))) * 5
+        lines.append((springs, record))
+
+cache = {}
+def rec_find_number(springs, record, position, current_group_index):
+
+
+"""total = 0
+for springs, record in tqdm.tqdm(lines):
+    total += smartforce(springs, record, springs.count("?"))
+print(total)"""
