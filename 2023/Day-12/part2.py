fe0e30fa8593f3f23832f6ccb320a7e7fcf17ed4
Lienep0 <neilp@live.fr>
2025-12-15 14:46:44 +0100

    Day 12 (Can still optimize)

--- 2023/Day-12/part2.py
+++ 2023/Day-12/part2.py
@@ -1,18 +1,74 @@
-import tqdm
+import sys
+from functools import cache
+
+sys.setrecursionlimit(2 ** 30 - 1)
 
 lines = []
-with open("easyinput.txt", "r") as f:
+with open("input.txt", "r") as f:
     for line in f.readlines():
         springs, record = line.strip().split(" ")
         springs = (springs + "?") * 4 + springs
         record = tuple(map(int, record.split(','))) * 5
         lines.append((springs, record))
 
-cache = {}
-def rec_find_number(springs, record, position, current_group_index):
+# Heuristic to make this faster : stop when the remaining number of ? is less than the sum of the remaining groups
+def find_number(springs, record):
+    @cache
+    def rec_find_number(position, group_index, group_size, on_group):
+        # base case
+        if position == len(springs):
+            if on_group:
+                return int(group_index == len(record) - 1 and group_size == record[-1])
+            else:
+                return int(group_index == len(record))
 
+        if springs[position] == "?":
+            if on_group:
+                # continue group
+                if group_size < record[group_index]:
+                    v = rec_find_number(position + 1, group_index, group_size + 1, True)
+                # interrupt group
+                else:
+                    v = rec_find_number(position + 1, group_index + 1, 0, False)
+            else:
+                # Make a choice : start a group or not
+                # if we've done all groups, there's no choice:
+                if group_index == len(record):
+                    v = rec_find_number(position + 1, group_index, 0, False)
+                else:
+                    v = (rec_find_number(position + 1, group_index, 1, True)
+                        + rec_find_number(position + 1, group_index, 0, False))
+        elif springs[position] == ".":
+            # interrupt group
+            if on_group:
+                # if the group size is too low, BEEP BEEP
+                if group_size < record[group_index]:
+                    v = False
+                else:
+                    v = rec_find_number(position + 1, group_index + 1, 0, False)
+            # just continue without a group
+            else:
+                v = rec_find_number(position + 1, group_index, 0, False)
+        elif springs[position] == "#":
+            if on_group:
+                # continue group
+                if group_size < record[group_index]:
+                    v = rec_find_number(position + 1, group_index, group_size + 1, True)
+                # BEEP BEEP WE ARE NOT GOOD
+                else:
+                    v = 0
+            # start a new group
+            else:
+                # if we've done all groups, BEEP BEEP
+                if group_index == len(record):
+                    v = 0
+                else:
+                    v = rec_find_number(position + 1, group_index, 1, True)
+        return v
 
-"""total = 0
-for springs, record in tqdm.tqdm(lines):
-    total += smartforce(springs, record, springs.count("?"))
-print(total)"""
+    return rec_find_number(0, 0, 0, False)
+
+total = 0
+for i in range(len(lines)):
+    total += find_number(*lines[i])
+print(total)
